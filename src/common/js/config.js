3c0154d7545c71a78a314d847345647887c23881
Rotaryz <1016994190@qq.com>
2018-02-01 18:18:25 +0800

    修改账户静态页面

--- src/common/js/config.js
+++ src/common/js/config.js
@@ -5,7 +5,7 @@ import axios from 'axios'
 import { BASE_URL } from 'api/config'
 
 export function initAxios() {
-  let authorization = localStorage.getItem('token')
+  let authorization = localStorage.getItem('token') || sessionStorage.getItem('token')
   axios.defaults.headers.common['Authorization'] = authorization
   axios.defaults.baseURL = BASE_URL.api
 }
