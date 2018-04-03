c65825b15593aa934ac8e09e1282703a9aa5ef85
ben <xuedengheng@qq.com>
2018-04-03 18:42:44 +0800

    越权+二维码预览

--- src/api/config.js
+++ src/api/config.js
@@ -12,7 +12,6 @@ const PROD_URL = {
   api: 'https://admin-api.jkweixin.com'
 }
 
-// export const BASE_URL = env.NODE_ENV === 'production' ? PROD_URL : env.NODE_ENV === 'test' ? TEST_URL : DEV_URL
-export const BASE_URL = TEST_URL
+export const BASE_URL = env.NODE_ENV === 'production' ? PROD_URL : env.NODE_ENV === 'test' ? TEST_URL : DEV_URL
 
 export const ERR_OK = 0
