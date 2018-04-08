ee35ad51976d619c81f3bcbaffd5047c4d23d231
Rotaryz <1016994190@qq.com>
2018-04-08 10:32:18 +0800

    整理代码

--- src/page/login/login.vue
+++ src/page/login/login.vue
@@ -43,7 +43,7 @@
       }
     },
     created() {
-      let token = localStorage.getItem('token') ? localStorage.getItem('token') : sessionStorage.getItem('token') ? sessionStorage.getItem('token') :false
+      let token = localStorage.getItem('token') ? localStorage.getItem('token') : sessionStorage.getItem('token') ? sessionStorage.getItem('token') : false
       if (token) {
         location.href = '#/container/data'
       }
