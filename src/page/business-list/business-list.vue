9b580cbb257af855d7b67ea2ed1c6b344f9b8fb3
Rotary <849307221@qq.com>
2018-04-18 14:02:50 +0800

    商户列表页码归1

--- src/page/business-list/business-list.vue
+++ src/page/business-list/business-list.vue
@@ -23,7 +23,7 @@
             <div class="list-item list-text">{{item.service_version ? '基础' : '试用'}}</div>
             <div class="list-item list-text">{{item.is_expiration ? '已过期' : '使用中'}}</div>
             <div class="list-item list-text">
-              {{item.is_checked === 2 ? '已认证' : '未认证'}}
+              {{item.is_checked === 2 ? '已认证' : '未认证 '}}
               <!--{{item.open_type === 0 ? '支付开通' : item.open_type === 1 ? '盟主开通' : item.open_type === 2 ? '后台开通' : ''}}-->
             </div>
             <div class="list-item">
