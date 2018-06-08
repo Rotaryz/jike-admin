511975572a685ac54a40d265598ffd81258d8ad2
zhouxuan@jkweixin.com <849307221@qq.com>
2018-06-08 11:52:41 +0800

    修改状态

--- src/page/monies-inquiry/monies-inquiry.vue
+++ src/page/monies-inquiry/monies-inquiry.vue
@@ -72,7 +72,7 @@
           <!--异联卡报名-->
           <div class="list-item list-text" v-if="item.order_type === '14'">{{item.status === 0 ? '待付款' : item.status === 1 ? '已付款' : item.status === 7 ? '退款中' : item.status === 3 ? '已退款' : item.status === 6 ? '已关闭' : ''}}</div>
           <!--买单-->
-          <div class="list-item list-text" v-if="item.order_type === '5'">{{item.status === 0 ? '待支付' :  '已支付'}}</div>
+          <div class="list-item list-text" v-if="item.order_type === '5'">{{item.status === 0 ? '待支付' : item.status === 1 ? '已支付' : '已关闭'}}</div>
           <!--联盟-->
           <div class="list-item list-text" v-if="item.order_type === '1' || item.order_type === '4'">{{item.status === 0 ? '未处理' : item.status === 1 ? '受理成功' : item.status === 2 ? '审核不通过' : item.status === 3 ? '提现成功' : '过期退款'}}</div>
           <div class="list-item list-text" v-if="item.order_type === '6'">{{item.status === 0 ? '待支付' : item.status === 1 ? '已支付' : item.status === 2 ? '商家已确认' : item.status === 3 ? '拒绝后已退款' : '过期退款'}}</div>
@@ -190,7 +190,7 @@
   const GROUNDLIST = [{title: '全部', status: ''}, {title: '待支付', status: 0}, {title: '已支付', status: 1}, {title: '待评价', status: 2}, {title: '退款中', status: 3}, {title: '退款完成', status: 4}, {title: '已完成', status: 5}, {title: '逾期付款已关闭', status: 6}, {title: '退款失败商家余额不足', status: 7}, {title: '退款失败平台余额不足', status: 8}, {title: '有效期过期关闭', status: 9}, {title: '待成团', status: 10}]
   const DEPOSIT = [{title: '全部', status: ''}, {title: '未处理', status: 0}, {title: '受理成功', status: 1}, {title: '审核不通过', status: 2}, {title: '提现成功', status: 3}, {title: '提现失败', status: 4}]
   const ALL = [{title: '全部', status: ''}]
-  const PAY = [{title: '全部', status: ''}, {title: '待支付', status: 0}, {title: '已支付', status: 1}]
+  const PAY = [{title: '全部', status: ''}, {title: '待支付', status: 0}, {title: '已支付', status: 1}, {title: '已关闭', status: 6}]
   /* 异联卡购买订单状态 */
   const YLBUY = [{title: '全部', status: ''}, {title: '待支付', status: 0}, {title: '待使用', status: 1}, {title: '已使用', status: 2}, {title: '已完成', status: 5}, {title: '已关闭', status: 6}]
   /* 异联卡报名 */
